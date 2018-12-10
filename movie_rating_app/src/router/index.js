e4ccdc1909ad35c36410a5d0f79c12b10c6e1a65
韦粤信 <283940802@qq.com>
2018-12-10 13:23:10 +0800

    feature: build single Movie view with id

--- movie_rating_app/src/router/index.js
+++ movie_rating_app/src/router/index.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Contact from '@/components/Contact'
+//import Contact from '@/components/Contact'
 import Home from '@/components/Home'
 import AddMovie from '@/components/AddMovie'
+import Movie from '@/components/Movie';
+//import Register from '@/components/Register';
+//import Login from '@/components/Login';
 Vue.use(Router)
 
 export default new Router({
@@ -20,6 +23,11 @@ export default new Router({
             path: '/movies/add',
             name: 'AddMovie',
             component: AddMovie
+        },
+        {
+            path: '/movies/:id',
+            name: 'Movie',
+            component: Movie,
         }
     ],
     mode: 'history',
