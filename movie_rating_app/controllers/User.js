966881d5df2e9956386119492365d3bc44b85a10
韦粤信 <283940802@qq.com>
2018-12-12 20:48:08 +0800

    feature: modify controller user.js with passport-local

--- movie_rating_app/controllers/User.js
+++ movie_rating_app/controllers/User.js
@@ -1,4 +1,8 @@
 const User = require('../models/User.js');
+//12.12 edit import passport
+const passport = require('passport');
+const LocalStrategy = require('passport-local')
+    //
 const passportJWT = require('passport-jwt');
 const jwt = require('jsonwebtoken');
 const extractJwt = passportJWT.ExtractJwt;
@@ -6,47 +10,52 @@ const jwtOptions = {};
 jwtOptions.jwtFromRequest = extractJwt.fromAuthHeaderWithScheme('jwt');
 jwtOptions.secretOrKey = "thisisthesecretkey";
 module.exports.controller = (app) => {
-
-    app.post('/users/register', (req, res) => {
-            const name = req.body.name;
-            const email = req.body.email;
-            const password = req.body.password;
-            const newUser = new User({
-                name,
-                email,
-                password,
+    passport.use(new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+    }, (email, password, done) => {
+        User.getUserByEmail(email, (err, user) => {
+            if (err) { return done(err) }
+            if (!user) { return done(null, false) }
+            User.comparePassword(password, user.password, (error, isMatch) => {
+                if (isMatch) {
+                    return done(null, user);
+                }
+                return done(null, false);
             });
-            User.createUser(newUser, (error, user) => {
-                if (error) {
-                    console.log("in controller user.js createUser:", error)
-                    res.status(422).json({ message: 'something went wrong,try again on sometime' })
-                }
-                res.send({ user })
-            })
+            return true;
         })
-        //login a user
-    app.post('/users/login', (req, res) => {
-        if (req.body.email && req.body.password) {
-            const email = req.body.email;
-            const password = req.body.password;
-            User.getUserByEmail(email, (err, user) => {
-                if (!user) {
-                    console.log("in controller user,not found");
-                    res.status(404).json({ message: 'The user does not exist!' });
-                } else {
-
-                    User.comparePassword(password, user.password, (error, isMatch) => {
-                        if (error) { throw error }
-                        if (isMatch) {
-                            const payload = { id: user.id };
-                            const token = jwt.sign(payload, jwtOptions.secretOrKey);
-                            res.json({ message: 'ok', token })
-                        } else {
-                            res.status(401).json({ message: 'The password is incorrect' });
-                        }
-                    })
-                }
-            })
+    }));
+    //login a user
+    app.post('users/login', passport.authenticate('local', { failureRedirect: '/users/login' }),
+        (req, res) => {
+            res.redirect('/');
         }
+    )
+    passport.serializeUser((user, done) => {
+        done(null, user.id);
+    });
+    passport.deserializeUser((id, done) => {
+        User.findById(id, (error, user) => {
+            done(error, user);
+        })
     })
+    app.post('/users/register', (req, res) => {
+        const name = req.body.name;
+        const email = req.body.email;
+        const password = req.body.password;
+        const newUser = new User({
+            name,
+            email,
+            password,
+        });
+        User.createUser(newUser, (error, user) => {
+            if (error) {
+                console.log("in controller user.js createUser:", error)
+                res.status(422).json({ message: 'something went wrong,try again on sometime' })
+            }
+            res.send({ user })
+        })
+    })
+
 }
