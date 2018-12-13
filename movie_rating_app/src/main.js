52f6633efafa65faa699f734bd326490989d317e
韦粤信 <283940802@qq.com>
2018-12-13 13:46:27 +0800

    feature: import vuex 's store

--- movie_rating_app/src/main.js
+++ movie_rating_app/src/main.js
@@ -8,7 +8,7 @@ import Vuetify from 'vuetify';
 import VueSwal from 'vue-swal';
 import App from './App'
 import router from './router'
-
+import { store } from './store/store.js';
 
 Vue.use(BootstrapVue);
 Vue.use(Vuetify);
@@ -18,6 +18,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
+    store,
     router,
     components: { App },
     template: '<App/>'
